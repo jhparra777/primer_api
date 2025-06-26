305d1502ac2c3a479cf409733ca324ebeec8e413
Jesus Parra <parra.jesus@gmail.com>
2025-06-25 20:37:53 -0400

    Corrigiendo .gitignore

---
+++ db/database.py
@@ -0,0 +1,13 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+sqliteName = 'movies.sqlite'
+base_dir = os.path.dirname(os.path.realpath(__file__))
+databaseURL = f'sqlite:///{os.path.join(base_dir, sqliteName)}'
+
+engine = create_engine(databaseURL, echo=True)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
